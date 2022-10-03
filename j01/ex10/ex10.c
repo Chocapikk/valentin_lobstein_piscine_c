f71ddc1da5c1f2d7bc18f88f4ee06bf319ded046
Chocapik <88535377+Chocapikk@users.noreply.github.com>
2022-10-03 22:25:20 +0200

    Update ex10.c

--- j01/ex10/ex10.c
+++ j01/ex10/ex10.c
@@ -1,92 +1,86 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 #include <string.h>
 
 
-double add(double a, double b){
-	return a + b;
+double add(double a, double b) {
+  return a + b;
 
 }
 
-double substract(double a , double b){
-	return a - b;
+double substract(double a, double b) {
+  return a - b;
 }
 
-double multiply(double a, double b){
-	return a * b;
+double multiply(double a, double b) {
+  return a * b;
 }
 
-double divide(double a, double b){
-	return a / b;
+double divide(double a, double b) {
+  return a / b;
 }
 
-void discard_junk () 
-{
+void discard_junk() {
   int c;
-  while((c = getchar()) != '\n' && c != EOF);
+  while ((c = getchar()) != '\n' && c != EOF);
 }
 
+void main() {
 
-void main(){
+  double a, b;
+  double result;
+  int choice = 0;
+  int i = 0;
+  char * congrat = "Good Value :)";
+  char * nope = "Please enter an integer hacker ~$ ";
+  char * choice_list = "\n1) Add\n2) Substract\n3) Multiply\n4) Divide\nChoose an option : ";
+  char * number_1 = "Choose number 1 ~$ ";
+  char * number_2 = "Choose number 2 ~$ ";
 
-	double a,b;
-	double result;
-	int choice = 0;
-	int i = 0;
-	char *congrat = "Good Value :)";
-	char *nope = "Please enter an integer hacker ~$ ";
-	char *choice_list = "\n1) Add\n2) Substract\n3) Multiply\n4) Divide\nChoose an option : ";
-	char *number_1 = "Choose number 1 ~$ ";
-	char *number_2 = "Choose number 2 ~$ ";
-	
-	write(1,number_1,strlen(number_1));
+  write(1, number_1, strlen(number_1));
 
-	do {
-    		i = scanf("%lf", &a);
-    		if (i == 1) {
-        		write(1,congrat,strlen(congrat));
-    		}
+  do {
+    i = scanf("%lf", & a);
+    if (i == 1) {
+      write(1, congrat, strlen(congrat));
+    } else {
+      write(1, nope, strlen(nope));
+      discard_junk();
+    }
 
-    		else {
-    			write(1,nope,strlen(nope));
-			discard_junk();
-		}
+  } while (i != 1);
 
-	} while(i != 1);
+  write(1, "\n", strlen("\n"));
 
-	write(1,"\n",strlen("\n"));
-	
-	write(1,number_2,strlen(number_2));
-	do {
-    		i = scanf("%lf", &b);
-      		if (i == 1) {
-        		write(1,congrat,strlen(congrat));
-      	}
+  write(1, number_2, strlen(number_2));
+  do {
+    i = scanf("%lf", & b);
+    if (i == 1) {
+      write(1, congrat, strlen(congrat));
+    } else {
+      write(1, nope, strlen(nope));
+      discard_junk();
+    }
 
-    		else {
-    			write(1,nope,strlen(nope));
-			discard_junk();
-		}
+  } while (i != 1);
 
-	} while(i !=1);
+  write(1, "\n", strlen("\n"));
 
-    write(1,"\n",strlen("\n"));
-	
+  while (choice < 1 || choice > 5) {
+    write(1, choice_list, strlen(choice_list));
+    scanf("%d", & choice);
+  }
 
-	
-	while (choice < 1 || choice > 5){
-		write(1,choice_list,strlen(choice_list));
-		scanf("%d",&choice);
-	}
-
-	if(choice == 1)
-		result = add(a,b);
-	if (choice == 2)
-		result = substract(a,b);
-	if (choice == 3)
-		result = multiply(a,b);
-	if (choice == 4)
-		result = divide(a,b);
-	printf("Answer is : %lf\n",result);
+  if (choice == 1)
+    result = add(a, b);
+  if (choice == 2)
+    result = substract(a, b);
+  if (choice == 3)
+    result = multiply(a, b);
+  if (choice == 4)
+    result = divide(a, b);
+  printf("Answer is : %lf\n", result);
 
 }
