da90d617228dfab3ba1c1c79fd58af474fea6413
Chocapikk <balgogan@protonmail.com>
2022-10-03 15:03:55 +0200

    ex08

---
+++ j01/ex08/ex08.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+
+bool o_pow3(int n){
+	
+	if(n % 3 == 0)
+		return true;
+	return false;
+}
+
+
+
+
+
+
+
+void main(){
+
+bool result = o_pow3(3);
+bool result2 = o_pow3(8);
+bool result3 = o_pow3(159);
+
+}
